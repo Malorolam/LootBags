57a530227fa70dff39ec43baf720f024ccd89eb1
gigabit101 <thegigabit101@gmail.com>
2017-01-02 22:38:09 +0000

    Added Capability Wrapper to Tiles to allow pipes thet only use Capability's to insert/extract

--- mal/lootbags/tileentity/TileEntityRecycler.java
+++ mal/lootbags/tileentity/TileEntityRecycler.java
@@ -18,6 +18,9 @@ import net.minecraft.util.ITickable;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraft.world.World;
+import net.minecraftforge.common.capabilities.Capability;
+import net.minecraftforge.items.CapabilityItemHandler;
+import net.minecraftforge.items.wrapper.SidedInvWrapper;
 
 public class TileEntityRecycler extends TileEntity implements IInventory, ISidedInventory, ITickable{
 
@@ -334,6 +337,22 @@ public class TileEntityRecycler extends TileEntity implements IInventory, ISided
 
 	@Override
 	public void clear() {
+	}
+
+	@Override
+	public boolean hasCapability(Capability<?> capability, EnumFacing facing) {
+		if(capability == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY) {
+			return true;
+		}
+		return super.hasCapability(capability, facing);
+	}
+
+	@Override
+	public <T> T getCapability(Capability<T> capability, EnumFacing facing) {
+		if(capability == CapabilityItemHandler.ITEM_HANDLER_CAPABILITY) {
+			return (T) new SidedInvWrapper(this, facing);
+		}
+		return super.getCapability(capability, facing);
 	}
 
 }
